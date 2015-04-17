2575448aced4f28bf4ad2290f50e39d3482a3225
dalecgu <131250169@smail.nju.edu.cn>
2015-04-18 00:08:20 +0800

    test

--- index.php
+++ index.php
@@ -1,1 +1,98 @@
-<html><body><h1>test</h1></body></html>
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 //EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<title>考试帮</title>
+		<link rel="Stylesheet" href="main.css" type="text/css" />
+	</head>
+	<body>
+		<div id="main">
+			<table width="100%">
+				<tr>
+					<td align="left" width="25%">
+						<h1>考试帮</h1>
+					</td>
+					<td width="50%">
+						<form action="search.php" method="post">
+							<table width="100%">
+								<tr>
+									<td width="50%">
+										<input type="text" size="50" name="search" value="南京大学 计算系统基础 王浩然" onclick="if(value==defaultValue){value='';this.style.color='#000'}" onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999" />
+									</td>
+									<td align="left">
+										<input type="submit" name="search" value="搜索"/>
+									</td>
+								</tr>
+							</table>
+						</form>
+					</td>
+					<td width="25%">
+						<form action="login.php" method="post">
+							<table width="100%" style="border-left:1px solid white">
+								<tr>
+									<th align="right" style="color:#FFFFFF">用户名:</th>
+									<td align="left">
+										<input type="text" size="20" name="username"/>
+									</td>
+									<td align="center">
+										<a href="rigister.php" style="text-decoration:none;"><input type="button" name="rigister" value="注册"/></a>
+									</td>
+								</tr>
+								<tr>
+									<th align="right" style="color:#FFFFFF">密码:</th>
+									<td align="left">
+										<input type="password" size="20" name="password"/>
+									</td>
+									<td align="center">
+										<input type="submit" name="login" value="登陆"/>
+									</td>
+								</tr>
+							</table>
+						</form>
+					</td>
+				</tr>
+			</table>
+			<hr/>
+			<div id="entries-area">
+				<table width="100%" border="1" cellspacing="0">
+					<tr>
+						<td align="center" valign="top" rowspan="2">
+							123赞
+						</td>
+						<td>
+							<table width="100%" border="1" cellspacing="0">
+								<tr>
+									<td align="center" width="20%" bgcolor="#DBDBFF">南京大学</td>
+									<td align="center" width="20%" bgcolor="#DBDBFF">计算系统基础</td>
+									<td align="center" width="20%" bgcolor="#DBDBFF">王浩然</td>
+									<td align="center" width="20%" bgcolor="#DBDBFF">期末复习</td>
+									<td align="center" width="20%" bgcolor="#DBDBFF">admin</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							文件预览<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<hr/>
+			<table width="100%" height="100">
+				<tr>
+					<td align="center" style="color:#FFFFFF">
+						Copyright (C) WME, All Rights Reserved
+					</td>
+				</tr>
+				<tr>
+					<td align="center">
+						<a href="mailto:wmegroup@163.com" style="color:#FFFFFF">联系方式：wmegroup@163.com</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
